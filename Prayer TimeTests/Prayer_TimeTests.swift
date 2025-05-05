a13448cc5dc14c008904868175773d51142913cb
Ahmed S. <ahmedsaheed2@outlook.com>
2025-05-05 01:00:24 +0100

    Once upon a commit...

---
+++ Prayer TimeTests/Prayer_TimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Prayer_TimeTests.swift
+//  Prayer TimeTests
+//
+//  Created by Ahmed, Ahmed on 04/05/2025.
+//
+
+import Testing
+@testable import Prayer_Time
+
+struct Prayer_TimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
